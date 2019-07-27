a3f70e04c189a6a8ff84cc856f21b4515cef4b95
DevilNinja1995 <Farhat_ekhlas@outlook.com>
2019-07-27 14:19:38 +0200

    hello

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,24 @@
 
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <limits>
+
+using namespace std;
+
 
 int main(){
 
-    std::cout << "Hello World" << std::endl;
+    string miles;
+    double mile, kilometers;
+
+    cout << "Enter miles: ";
+    getline(cin, miles);
+    mile = stod(miles);
+    kilometers = mile * 1.60934;
+    printf("%.1f miles equals %.4f", mile, kilometers);
+    
     return 0;
 }
