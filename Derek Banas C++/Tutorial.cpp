5b2745764c66ecc20af4025ae9fca653f36ae142
DevilNinja1995 <Farhat_Ekhlas@outlook.com>
2019-08-05 12:37:30 +0200

    Nothing

---
+++ Derek Banas C++/Tutorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <limits>
+#include <numeric>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
